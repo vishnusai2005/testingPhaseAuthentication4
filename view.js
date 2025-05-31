522f8ed3e10f9d8bbceb4d04fab1cc67c38be1e4
VYDHYAM VISHNUSAI <23102A010034@mbu.asia>
2025-05-31 19:03:19 +0530

    Create view.js

---
+++ view.js
@@ -0,0 +1,5 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', {value: true})
+const Render_1 = require('./Render')
+Render_1.Render.init()
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmlldy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9yZW5kZXIvdmlldy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHFDQUFrQztBQUNsQyxlQUFNLENBQUMsSUFBSSxFQUFFLENBQUMifQ==
